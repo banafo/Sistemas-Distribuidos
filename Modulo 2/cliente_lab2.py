eb102a58851d854e553eee14559c8fd69785f3dd
banafo <banafo.ab@gmail.com>
2022-04-27 20:57:26 -0300

    Update cliente_lab2.py

--- Modulo 2/cliente_lab2.py
+++ Modulo 2/cliente_lab2.py
@@ -1,26 +1,21 @@
 import socket
 
-HOST = 'localhost'   # maquina onde esta o par passivo
-PORTA = 5000        # porta que o par passivo esta escutando
+HOST = 'localhost'   						# maquina onde esta o par passivo
+PORTA = 5000       						# porta que o par passivo esta escutando
 
-# cria socket
-sock = socket.socket() # default: socket.AF_INET, socket.SOCK_STREAM 
+sock = socket.socket() 						# cria socket //default: socket.AF_INET, socket.SOCK_STREAM 
+sock.connect((HOST, PORTA))					# conecta-se com o par passivo
 
-# conecta-se com o par passivo
-sock.connect((HOST, PORTA))
 print('Digite "pare" se deseja encerra ou pode continua com a sua mensagem.\n')
 
 while True:
 	msg_to_server = input('Digite o nome do arquivo para servidor: ')
-	if msg_to_server == 'pare':break 	#end program
+	if msg_to_server == 'pare':break 							#end program
 	else:
-		# envia uma mensagem para o par conectado
-		sock.send(bytes(msg_to_server, 'UTF-8'))  #convert to bytes
+		sock.send(bytes(msg_to_server, 'UTF-8'))  					# envia uma mensagem para o par conectado //convert to bytes
 		#espera a resposta do par conectado (chamada pode ser BLOQUEANTE)
-		msg_from_server = sock.recv(1024) # argumento indica a qtde maxima de bytes da mensagem
-		# imprime a mensagem recebida do servidor
-		print('Retorno do servidor: ' + str(msg_from_server,  encoding='utf-8'))
+		msg_from_server = sock.recv(1024) 						# argumento indica a qtde maxima de bytes da mensagem
+		print('Retorno do servidor: ' + str(msg_from_server,  encoding='utf-8')) 	# imprime a mensagem recebida do servidor
 		
-# encerra a conexao
-sock.close() 
+sock.close() 	# encerra a conexao
 
